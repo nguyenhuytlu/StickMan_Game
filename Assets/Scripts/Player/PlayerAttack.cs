08c085950a1fd7f6459273919991c5c01de08f01
HungHoang1 <2051063705@e.tlu.edu.vn>
2023-12-14 21:37:20 +0700

    fix conflict

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -7,6 +7,7 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField]private float attackCooldown;
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject[] fireBalls;
+    [SerializeField] private AudioClip fireballSound;
 
 
     private Animator anim;
@@ -28,6 +29,7 @@ public class PlayerAttack : MonoBehaviour
     }
     private void Attack()
     {
+        SoundManager.instance.PlaySound(fireballSound);
 
         anim.SetTrigger("attack");
         cooldownTimer = 0;
