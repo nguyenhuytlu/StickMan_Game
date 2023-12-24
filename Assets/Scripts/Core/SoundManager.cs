66431bc30ce5e08e7f1c02121764035cdfe005f4
dinhhung202 <99945877+dinhhung202@users.noreply.github.com>
2023-12-24 23:11:06 +0700

    thêm menu+pause menu

--- Assets/Scripts/Core/SoundManager.cs
+++ Assets/Scripts/Core/SoundManager.cs
@@ -14,9 +14,47 @@ public class SoundManager : MonoBehaviour
     {
         instance = this;
         source = GetComponent<AudioSource>();
+        soundSource = GetComponent<AudioSource>();
+        musicSource = GetComponent<AudioSource>();
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != null && instance != this)
+            Destroy(gameObject);
+
+        ChangeMusicVolume(0);
+        ChangeSoundVolume(0);
     }
     public void PlaySound(AudioClip _sound)
     {
         source.PlayOneShot(_sound);
+    }
+    public void ChangeSoundVolume(float _change)
+    {
+        ChangeSourceVolume(1, "soundVolume", _change, soundSource);
+    }
+
+    public void ChangeMusicVolume(float _change)
+    {
+        ChangeSourceVolume(0.3f, "musicVolume", _change, musicSource);
+    }
+
+    private void ChangeSourceVolume(float baseVolume, string volumeName, float change, AudioSource source)
+    {
+        float currentVolume = PlayerPrefs.GetFloat(volumeName, 1);
+        currentVolume += change;
+
+        if (currentVolume > 1)
+            currentVolume = 0;
+        else if (currentVolume < 0)
+            currentVolume = 1;
+
+        float finalVolume = currentVolume * baseVolume;
+        source.volume = finalVolume;
+
+        PlayerPrefs.SetFloat(volumeName, currentVolume);
     }
 }
